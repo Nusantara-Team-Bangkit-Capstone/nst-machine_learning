9d5bb5aaa20e24f94fcca5474d032cd976bd9d88
renjer pink <renjerpink@pop-os.localdomain>
2023-06-10 20:05:47 +0800

    update: connect to Cloud Storage

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import get_file
 from tensorflow.keras.preprocessing import image
-from google.auth import credentials
 from google.cloud import storage
+from datetime import datetime
 
 
 import os
@@ -21,7 +22,6 @@ def index_view():
     return 'API is Running'
 
 
-# Allow files with extension png, jpg and jpeg
 ALLOWED_EXT = set(['jpg', 'jpeg', 'png'])
 
 
@@ -29,39 +29,54 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXT
 
-# Function to load and prepare the image in right shape
-
 
 def read_image(filename):
-    img = load_img(filename, target_size=(150, 150))
+    image_url = get_file(origin=filename)
+    img = load_img(image_url, target_size=(150, 150))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = np.vstack([x])
     return x
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/api/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         file = request.files['file']
 
-        # Create a Cloud Storage client.
-        storage_client = storage.Client()
+        storage_client = storage.Client.from_service_account_json(
+            'serviceaccountkey.json')
 
-        # Get the bucket that the file will be uploaded to.
         bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
 
-        # Create a new blob and upload the file's content.
-        blob = bucket.blob(file.filename)
-        blob.upload_from_string(file.read(), content_type=file.content_type)
-
         if file and allowed_file(file.filename):
-            # filename = file.filename
-            img_url = "gs://{}/{}".format(CLOUD_STORAGE_BUCKET, blob.name)
-            # file_path = os.path.join('static/images', filename)
-            # file.save(file_path)
-            # img = read_image(file_path)
-            class_prediction = model.predict(img_url)
+
+            getDate = datetime.now()
+
+            file.filename = os.path.join(getDate.strftime(
+                "%d_%m_%Y_%H_%M_%S") + '.' + file.filename.rsplit('.', 1)[1])
+
+            file_path = os.path.join('static/images', file.filename)
+            file.save(file_path)
+
+            blob = bucket.blob(file.filename)
+            blob.upload_from_filename(
+                os.path.join('static/images', file.filename))
+
+            # remove image after upload to Cloud Storage
+            os.remove(os.path.join('static/images', file.filename))
+
+            img_url = "https://storage.googleapis.com/{}/{}".format(
+                CLOUD_STORAGE_BUCKET, file.filename)
+
+            print(img_url)
+
+            remove_white_space = img_url.replace(" ", "%20")
+
+            img = read_image(remove_white_space)
+
+            class_prediction = model.predict(img)
+
             if class_prediction == 0:
                 result = "Gigi Sehat"
             elif class_prediction == 1:
